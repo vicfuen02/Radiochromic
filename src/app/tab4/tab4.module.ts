6d15439e2b47fec0b24e9f0d5b9516145a889de4
vicfuen <vicfuen02@gmail.com>
2020-12-30 16:17:18 +0100

    tab4 arreglado. Añadido a tab4.module el modulo explorecontainer que requería el tab4.html para mostrar UI explorer

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
 import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 import { Tab4Page } from './tab4.page';
@@ -13,7 +13,9 @@ import { Tab4Page } from './tab4.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab4PageRoutingModule
+    ExploreContainerComponentModule,
+    Tab4PageRoutingModule,
+    RouterModule.forChild([{ path: '', component: Tab4Page }]),
   ],
   declarations: [Tab4Page]
 })
